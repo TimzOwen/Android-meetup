43319af702a10912f55b8ebf213a588e47b5549d
Timz Owen <timzowen@Timz.local>
2022-07-06 22:59:51 +0300

    using data binding

--- AboutMe/app/src/main/java/com/timzowen/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/timzowen/aboutme/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import com.timzowen.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,24 +13,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.btnDone.setOnClickListener {
             addNickName(it)
         }
     }
-    //
+
     private fun addNickName(view : View){
-        binding.tvNickName.text = binding.etNickName.text
-        binding.etNickName.visibility = View.GONE
-        // hide button visibility
-        view.visibility = View.GONE
-        // un-hide text to display the given name
-        binding.tvNickName.visibility = View.VISIBLE
+        binding.apply {
+            binding.tvNickName.text = binding.etNickName.text
+            invalidateAll()
+            binding.etNickName.visibility = View.GONE
+            // hide button visibility
+            view.visibility = View.GONE
+            // un-hide text to display the given name
+            binding.tvNickName.visibility = View.VISIBLE
+        }
 
         // hide the keyboard input type
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+       val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken,0)
     }
+
 }
