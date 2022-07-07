e6e19282a69465c6ab2ee387a90ec939716de7fe
Timz Owen <timzowen@Timz.local>
2022-07-07 18:49:27 +0300

    cliackable views

--- ColorMyViews/app/src/main/java/com/timzowen/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/timzowen/colormyviews/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.timzowen.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three,
+                box_four, box_five)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.bnt_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
