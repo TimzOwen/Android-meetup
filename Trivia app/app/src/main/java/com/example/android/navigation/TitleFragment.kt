fe249b4430928f2120ead718194dc0277afedd4a
Timz Owen <timzowen@Timz.local>
2022-07-08 06:02:24 +0300

    fragment title added.

---
+++ Trivia app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,18 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+    // Instantiate a binding util class .
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,R.layout.fragment_title,container,false)
+        return binding.root
+    }
+
+}
