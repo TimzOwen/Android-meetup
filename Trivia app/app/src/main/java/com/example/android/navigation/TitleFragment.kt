b3b308be63462e43bbb8d695f2b5817baa18c2b9
TimzOwen <timzowen@gmail.com>
2022-07-08 06:55:00 +0300

    updated navigations

--- Trivia app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     // Instantiate a binding util class .
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,R.layout.fragment_title,container,false)
+
+        binding.playButton.setOnClickListener{view : View ->
+            Navigation.findNavController(view).navigate(R.id.gameFragment)
+        }
+
         return binding.root
     }
 
