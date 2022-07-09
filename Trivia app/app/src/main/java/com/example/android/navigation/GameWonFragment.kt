f1dc96d59697ccacb63a88fa673b2d5fce55ba9f
TimzOwen <timzowen@gmail.com>
2022-07-09 22:32:36 +0300

    menus

--- Trivia app/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Trivia app/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -3,12 +3,10 @@ package com.example.android.navigation
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
