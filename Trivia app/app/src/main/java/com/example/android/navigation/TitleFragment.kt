245e32e84531ada3c649ff57c6d995258ec2419c
TimzOwen <timzowen@gmail.com>
2022-07-09 14:59:30 +0300

    Menus for android navigation

--- Trivia app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia app/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -18,7 +18,16 @@ class TitleFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.gameFragment)
         )
 
+        setHasOptionsMenu(true) // to respond to the options menu on the action bar
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 }
