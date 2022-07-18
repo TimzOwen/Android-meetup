3b913181878290e04370a112d6a22223a4066e19
TimzOwen <timzowen@gmail.com>
2022-07-18 06:00:14 +0300

    code refactor

--- RoomAuthentication/app/src/main/java/com/timzowen/roomauthentication/data/UserDao.kt
+++ RoomAuthentication/app/src/main/java/com/timzowen/roomauthentication/data/UserDao.kt
@@ -1,10 +1,12 @@
 package com.timzowen.roomauthentication.data
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
