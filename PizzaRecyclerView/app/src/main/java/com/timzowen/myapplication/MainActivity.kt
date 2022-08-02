81b2dc2726d857f29c30fc106abdf10313ee940a
TimzOwen <timzowen@gmail.com>
2022-08-02 16:21:04 +0300

    implemented recycler view

--- PizzaRecyclerView/app/src/main/java/com/timzowen/myapplication/MainActivity.kt
+++ PizzaRecyclerView/app/src/main/java/com/timzowen/myapplication/MainActivity.kt
@@ -2,10 +2,47 @@ package com.timzowen.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.timzowen.myapplication.adapters.FoodAdapter
+import com.timzowen.myapplication.data.Food
+import com.timzowen.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var foodList : ArrayList<Food>
+    private lateinit var foodAdapter : FoodAdapter
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        foodList = ArrayList()
+        foodAdapter = FoodAdapter(foodList)
+        foodListItems()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.adapter = foodAdapter
+
+    }
+    private fun foodListItems(){
+        foodList.add(Food(R.drawable.pizza1,"chicken Pizza"))
+        foodList.add(Food(R.drawable.pizza2,"Beef Chicken"))
+        foodList.add(Food(R.drawable.noodles1,"normal Pizza"))
+        foodList.add(Food(R.drawable.noodles2,"sweet Chicken"))
+        foodList.add(Food(R.drawable.paneer,"Mini Pizza"))
+        foodList.add(Food(R.drawable.pasta,"Extra Chicken"))
+        foodList.add(Food(R.drawable.piza_spagethi,"meat Pizza"))
+        foodList.add(Food(R.drawable.pizza12,"vegez Chicken"))
+        foodList.add(Food(R.drawable.pizza1,"chicken Pizza"))
+        foodList.add(Food(R.drawable.pizza2,"Beef Chicken"))
+        foodList.add(Food(R.drawable.noodles1,"normal Pizza"))
+        foodList.add(Food(R.drawable.noodles2,"sweet Chicken"))
+        foodList.add(Food(R.drawable.paneer,"Mini Pizza"))
+        foodList.add(Food(R.drawable.pasta,"Extra Chicken"))
+        foodList.add(Food(R.drawable.piza_spagethi,"meat Pizza"))
+        foodList.add(Food(R.drawable.pizza12,"vegez Chicken"))
     }
 }
--- PizzaRecyclerView/app/src/main/java/com/timzowen/myapplication/adapters/FoodAdapter.kt
+++ PizzaRecyclerView/app/src/main/java/com/timzowen/myapplication/adapters/FoodAdapter.kt
@@ -1,4 +1,4 @@
-package com.timzowen.myapplication.Adapters
+package com.timzowen.myapplication.adapters
 
 import android.view.LayoutInflater
 import android.view.View
