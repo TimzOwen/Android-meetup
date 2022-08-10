fbcfa468a0f2cc3cf22ba3ff6538297959a77715
TimzOwen <timzowen@gmail.com>
2022-08-10 11:32:18 +0300

    .

--- RoomAuthentication/app/src/main/java/com/timzowen/roomauthentication/fragments/list/ListFragment.kt
+++ RoomAuthentication/app/src/main/java/com/timzowen/roomauthentication/fragments/list/ListFragment.kt
@@ -13,7 +13,6 @@ class ListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,container : ViewGroup?, savedInstanceState: Bundle?) : View? {
 
         val view = inflater.inflate(R.layout.fragment_add,container,false)
-
         return view
     }
 
