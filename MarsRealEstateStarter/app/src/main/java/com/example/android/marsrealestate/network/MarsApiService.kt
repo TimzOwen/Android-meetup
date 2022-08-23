6bcb235a5db3804054ce5564cf4b4171f57d06c8
TimzOwen <timzowen@gmail.com>
2022-08-23 13:00:15 +0300

    retrofit builders:

--- MarsRealEstateStarter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstateStarter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,12 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
