8c8066fef3151eaee3a268dff3b3898b1bed8eee
TimzOwen <timzowen@gmail.com>
2022-10-03 16:18:11 +0300

    ui design

--- MvvmJava/app/src/main/java/com/timzowen/mvvmjava/MainActivity.java
+++ MvvmJava/app/src/main/java/com/timzowen/mvvmjava/MainActivity.java
@@ -1,14 +1,28 @@
 package com.timzowen.mvvmjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
---
+++ MvvmJava/app/src/main/java/com/timzowen/mvvmjava/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.timzowen.mvvmjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        },4000);
+
+    }
+}
