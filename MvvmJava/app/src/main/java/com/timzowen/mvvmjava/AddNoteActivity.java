dfd2d8842fb02367f6251ec74a4cd0838bc1fc2a
TimzOwen <timzowen@gmail.com>
2022-10-04 09:19:01 +0300

    dependencies & add note update

--- MvvmJava/app/src/main/java/com/timzowen/mvvmjava/AddNoteActivity.java
+++ MvvmJava/app/src/main/java/com/timzowen/mvvmjava/AddNoteActivity.java
@@ -2,16 +2,22 @@ package com.timzowen.mvvmjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
 
     private EditText mTitleEdit, mDescEdit;
     private Button mSaveBtn;
     private NumberPicker numberPicker;
+
+    public static final String EXTRA_TITLE = "com.timzowen.mvvmjava.EXTRA_TITLE";
+    public static final String EXTRA_DESC = "com.timzowen.mvvmjava.EXTRA_DESC";
+    public static final String EXTRA_PRIORITY = "com.timzowen.mvvmjava.EXTRA_PRIORITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,25 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPicker.setMinValue(1);
         numberPicker.setMinValue(10);
 
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_close_24);
+
+        mSaveBtn.setOnClickListener(view -> {
+            String title = mTitleEdit.getText().toString();
+            String desc = mDescEdit.getText().toString();
+            int priority = numberPicker.getValue();
+
+            if (title.trim().isEmpty() || desc.trim().isEmpty()){
+                Toast.makeText(this,"Empty fields not allowed",Toast.LENGTH_SHORT).show();
+            }
+
+            Intent dataIntent = new Intent();
+            dataIntent.putExtra(EXTRA_TITLE,title);
+            dataIntent.putExtra(EXTRA_DESC, desc);
+            dataIntent.putExtra(EXTRA_PRIORITY,priority);
+
+            setResult(RESULT_OK, dataIntent);
+            finish();
+        });
 
     }
 }
--- MvvmJava/app/src/main/java/com/timzowen/mvvmjava/MainActivity.java
+++ MvvmJava/app/src/main/java/com/timzowen/mvvmjava/MainActivity.java
@@ -1,28 +1,61 @@
 package com.timzowen.mvvmjava;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int ADD_NOTE_REQUEST_CODE = 1;
     RecyclerView recyclerView;
-    FloatingActionButton floatingActionButton;
+    FloatingActionButton fab;
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floatingActionButton = findViewById(R.id.floatingActionButton);
+        fab = findViewById(R.id.floatingActionButton);
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.hasFixedSize();
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(this, AddNoteActivity.class);
+            startActivityForResult(intent, ADD_NOTE_REQUEST_CODE);
+        });
+
+        noteViewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) ViewModelProvider
+                .AndroidViewModelFactory
+                .getInstance(this.getApplication()))
+                .get(NoteViewModel.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_NOTE_REQUEST_CODE && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String desc = data.getStringExtra(AddNoteActivity.EXTRA_DESC);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, desc, priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note Failed to save", Toast.LENGTH_SHORT).show();
+        }
     }
 }
